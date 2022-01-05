6b50f4b7cd749ad9885280fd510b25ef4178e310
liang <liang@unchartedworks.com>
2022-01-05 20:10:50 +0100

    Initial Commit

---
+++ PizzaOrdering/PizzaOrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaOrderingApp.swift
+//  PizzaOrdering
+//
+//  Created by liang on 2022-01-05.
+//
+
+import SwiftUI
+
+@main
+struct PizzaOrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
